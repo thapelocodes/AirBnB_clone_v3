554462fd0b82751b46ba224fd8766e59e95d9cf2
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2024-01-07 22:18:55 +0200

    fix __init__.py in views 1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """Initializes the app_views Blueprint"""
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
 from api.v1.views.index import *
-from api.v1.views.states import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
