9661ee6ebcdc067d520834d927b9bfcc0815aec4
thapelocodes <123243154+thapelocodes@users.noreply.github.com>
2025-03-16 19:23:18 +0000

    chore: update app ports

--- api/v1/app.py
+++ api/v1/app.py
@@ -36,5 +36,5 @@ Swagger(app)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
+    port = getenv('HBNB_API_PORT', default=5002)
     app.run(host, int(port), threaded=True)
